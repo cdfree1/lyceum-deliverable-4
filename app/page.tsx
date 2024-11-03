845d9504fe4bd2ef24913cd0378994a9b9af445f
Caleb Free <cdfree@unc.edu>
2024-11-02 22:13:47 -0400

    Progress on the navbar and page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './styles/page.module.css';
+import Navbar from '@/components';
 
 export default function Home() {
   return (
