c094edfaf90139380f38c39550288d7060d0b489
Caleb Free <cdfree@unc.edu>
2024-11-06 23:52:51 -0500

    Fixed Breed

--- app/breeds/[breed]/page.tsx
+++ app/breeds/[breed]/page.tsx
@@ -5,16 +5,11 @@ import { capitalizeFirstLetter } from '@/app/utils/capitalize';
 type BreedProps = {
   params: {
     breed: string;
+    fact: string;
   };
 };
 
 const breeds = ['Labrador', 'Pomeranian', 'Poodle'];
-
-const breedFacts: Record<string, string> = {
-  Labrador: 'Labradors are known for their friendly and outgoing nature.',
-  Pomeranian: 'Pomeranians are small but have a big personality.',
-  Poodle: 'Poodles are highly intelligent and easy to train.',
-};
 
 export async function generateStaticParams() {
   return breeds.map((breed) => ({
@@ -32,9 +27,7 @@ export default async function BreedFacts({ params }: BreedProps) {
     <div>
       <Navbar />
       <div className="flex flex-col items-center pt-4">
-        <h1 className="text-green-500 font-extrabold text-3xl text-center">
-          {capitalizeFirstLetter(breed)} Facts
-        </h1>
+        <h1 className="text-green-500 font-extrabold text-3xl text-center">{capitalizeFirstLetter(breed)} Facts</h1>
         <Image src={image} alt={breed} width={300} height={300} className="rounded-lg shadow-lg pt-1" />
       </div>
     </div>
